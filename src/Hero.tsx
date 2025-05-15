2f4f69942a08e607d5bd968def70721507f03179
Philippe De Hovre <phil@Philippes-Mac-mini.local>
2025-05-15 17:11:22 +0200

    hero slogan placement

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,11 +1,21 @@
 import VideoPlayer from "./components/VideoPlayer"
+import './Hero.css'
 
 const Hero = () => {
 
     return (
         <div className="hero_ctn">
-
             <VideoPlayer />
+            <div className="slogan_ctn">
+                <div className="left top">
+                    <h1 id='phil'>Phil</h1>
+                    <h1 id='bass'>BASS</h1>
+                    <h1 id='plays'>plays</h1>
+                </div>
+                <div className="right bottom">
+                <h1>right - bottom</h1>
+                </div>
+            </div>
         </div>
     )
 }
